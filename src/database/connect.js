65fbc2c8cc043e4e7982c16e061a5aa7b075d0ce
Wyvig Israel <wyvigk5@gmail.com>
2022-11-05 13:09:56 -0300

    codigos de nodejs ainda nao finalizados

---
+++ src/database/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async () => {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.bjukwrz.mongodb.net/database?retryWrites=true&w=majority`,
+    (error) => {
+        if (error) {
+           return console.log("Ocorreu um error ao se conectar ao banco de dados: ",
+            error);
+        }
+
+        return console.log("Conexao ao banco de dados realizada com sucesso");
+    }
+  );
+};
+
+module.exports = connectToDatabase;
